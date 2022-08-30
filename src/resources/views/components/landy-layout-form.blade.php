370e0e240075002a095170699ce20b25b7468233
miftahurrahmi <miftahurrahmi45@gmail.com>
2022-08-30 17:07:32 +0700

    update seeder

--- src/resources/views/components/landy-layout-form.blade.php
+++ src/resources/views/components/landy-layout-form.blade.php
@@ -1,15 +1,3 @@
- {{-- <div class="container mx-auto max-w-[1280px] mt-10 lg:mt-16 lg:w-1/2 px-4">
-     <div class="card w-96 bg-base-100 shadow-xl">
-         <div class="card-body">
-            <p> You just got message from {{ $email }} </p>
-            <p> Subject : {{ $subject }} </p>
-            <p> Name    :{{ $name }} </p>
-            <p> Phone   :{{ $phone }} </p>
-            <p>Message  :{{ $messages }} </p>
-
-         </div>
-     </div>
- </div> --}}
 
  <!doctype html>
 <html style="color: black;">
